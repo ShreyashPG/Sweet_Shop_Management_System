ede814efff6039093bafbdfcd81e17fa42914246
Shreyash Ghanekar <shreyasghanekar35@gmail.com>
2025-09-18 11:17:58 +0530

    Setup connection for mongodb database

---
+++ backend/src/config/mongodb.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const mongoose =require("mongoose")
+
+/**
+ * Connects to the MongoDB database.
+ * This function should be called before starting the server.
+ */
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/incubytesweetdb");
+
+    console.log("✅ MongoDB connected successfully");
+     return "success"
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message); 
+    // process.exit(1); // Exit the process if DB connection fails
+  }
+};
+
+module.exports= connectToDatabase;

901605f766069facf750561d213d842b20e94408
Shreyash Ghanekar <shreyasghanekar35@gmail.com>
2025-09-18 11:29:24 +0530

    added routes for login and register

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const app=express();
 require("dotenv").config();
+const authRouter=require("./routes/Auth")
 
 const cors = require("cors");
 app.use(cors())
 app.use(express.json())
-
+app.use("/api/auth",authRouter)
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Server is running at http://localhost:5000`);
   });
---
+++ backend/src/routes/Auth.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+const router = express.Router();
+const Register=require("../controllers/Register")
+const login=require("../controllers/login")
+
+
+// Route: POST /api/auth/register
+
+router.post("/Register",Register);
+router.post("/login",login);
+
+module.exports = router;
+
